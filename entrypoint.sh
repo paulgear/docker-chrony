580f70d979105ad396830213eb0f0d99c3cc4374
Paul Gear <github@libertysys.com.au>
2020-12-11 11:15:02 +1000

    Update entrypoint.sh
    Obtain ssh authorized key from environment rather than build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 rm -fv /etc/ssh/ssh_host_*key*
+SSH=/root/.ssh
+mkdir $SSH
+chmod 700 $SSH
+echo "$SSH_AUTHORIZED_KEYS" > $SSH/authorized_keys
+chmod 600 $SSH/authorized_keys
 dpkg-reconfigure openssh-server
 service ssh start
 /usr/sbin/chronyd "$@"
