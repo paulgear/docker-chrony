984778c67768e0fe3cab77b02ae594e3c54f2fcb
Paul Gear <github@libertysys.com.au>
2021-12-30 15:53:45 +1000

    Move PTP device configuration to entrypoint
    Also tidy/comment entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-rm -fv /etc/ssh/ssh_host_*key*
+
+# install authorized_keys
 SSH=/root/.ssh
 mkdir $SSH
 chmod 700 $SSH
 echo "$SSH_AUTHORIZED_KEYS" > $SSH/authorized_keys
 chmod 600 $SSH/authorized_keys
+
+# set up ssh server
+rm -fv /etc/ssh/ssh_host_*key*
 dpkg-reconfigure openssh-server
 service ssh start
+
+# add PTP device to chrony configuration, if present
+if [ -e /dev/ptp0 ]; then
+    echo "refclock PHC /dev/ptp0 dpoll -2" >> /etc/chrony/chrony.conf
+fi
+
 /usr/sbin/chronyd "$@"
